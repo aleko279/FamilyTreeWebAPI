1834db5e21fa04063514754fa57bf19e94d20315
aleks <al057ex@gmail.com>
2025-04-11 11:11:53 +0400

    Add project files.

---
+++ FamilyTreeWebAPI/Models/Relationship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyTreeWebAPI.Models;
+
+public partial class Relationship
+{
+    public int Id { get; set; }
+
+    public int? Source { get; set; }
+
+    public int? Target { get; set; }
+}
---
+++ FamilyTreeWebAPI/Program.cs
@@ -0,0 +1,50 @@
+using FamilyTreeWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Newtonsoft.Json;
+using FamilyTreeWebAPI.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add database context
+builder.Services.AddDbContext<FamilyTreeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddControllersWithViews()
+//    .AddNewtonsoftJson(options =>
+//    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+//);
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    options.JsonSerializerOptions.MaxDepth = 32; // You can adjust this value if needed
+});
+// Add services to the container
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+builder.Services.AddScoped<FamilyTreeService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+// Use CORS
+app.UseCors("AllowAllOrigins");
+app.Run();
