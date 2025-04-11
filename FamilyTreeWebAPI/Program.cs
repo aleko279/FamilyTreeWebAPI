da69575dbe1239341b53063724821a4f7dfae302
aleks <al057ex@gmail.com>
2025-04-11 14:50:40 +0400

    modify swagger

--- FamilyTreeWebAPI/Program.cs
+++ FamilyTreeWebAPI/Program.cs
@@ -39,11 +39,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/familytreewebapi/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = "swagger";
-    });
+    app.UseSwaggerUI();
 }
 
 //app.UseHttpsRedirection();
